6f757991fc991b59ce99de444f4975d56a97329d
Yehor Petruk <yehor@juniqe.com>
2018-10-11 16:25:21 +0200

    Add patch todo route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {ObjectId} = require('mongodb');
+const _ = require('lodash');
 // requiring db models
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
@@ -66,6 +67,31 @@ app.delete('/todos/:id', (req, res) => {
       },
       (err) => res.status(400).send()
     );
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  console.log('updating object with', body);
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then(
+      (todo) => {
+        if (!todo) {
+          return res.status(404).send();
+        }
+        res.status(200).send({todo});
+      },
+      (err) => res.status(400).send()
+    )
 });
 
 app.listen(port, () => {
