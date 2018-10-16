ff8aba93bbadb85ab754e7a0b31b54c7284ceafe
Yehor Petruk <yehor@juniqe.com>
2018-10-16 14:41:14 +0200

    Add POST /users login route

--- server/models/user.js
+++ server/models/user.js
@@ -60,6 +60,25 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+  return User.findOne({email})
+    .then((user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, isCorrect) => {
+          if (isCorrect) {
+            return resolve(user);
+          }
+          return reject();
+        });
+      });
+      
+    });
+}
+
 UserSchema.methods.toJSON = function() {
   var user = this;
   var userObject = user.toObject();
--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {ObjectId} = require('mongodb');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 // requiring db models
 const {User} = require('./models/user');
@@ -122,6 +123,17 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send({user: req.user});
+});
+
+app.post('/users/login', (req, res) => {
+  const {email, password} = req.body;
+  User.findByCredentials(email, password)
+    .then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send({user});
+      });
+    })
+    .catch((err) => res.status(400).send());
 });
 
 app.listen(process.env.PORT, () => {
